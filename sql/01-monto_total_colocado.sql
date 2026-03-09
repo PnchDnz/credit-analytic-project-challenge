e1f2ca322cbf8e5e1ec6fc12e36a4c126eae97d1
Francisco Dominguez <DnzGo@Pancho-DnzGo.(none)>
2026-03-08 23:35:37 -0600

    Se agregan 2 validaciones mas al archivo 01-monto_total_colocado.sql

--- sql/01-monto_total_colocado.sql
+++ sql/01-monto_total_colocado.sql
@@ -5,3 +5,21 @@ SELECT DISTINCT
 FROM dim_credit
 ORDER BY 1
 ;
+
+/* Monto total colocado por mes y año */
+SELECT DISTINCT
+    TO_CHAR(origination_date, 'YYYY') AS anio,
+    TO_CHAR(origination_date, 'Month') AS mes,
+    SUM(amount) OVER (PARTITION BY TO_CHAR(origination_date, 'Month'),TO_CHAR(origination_date, 'YYYY')) AS total_colocado
+FROM dim_credit
+ORDER BY 1
+;
+
+/* Monto total colocado por mes para un año en especifico */
+SELECT DISTINCT
+    TO_CHAR(origination_date, 'Month') AS mes,
+    SUM(amount) OVER (PARTITION BY TO_CHAR(origination_date, 'Month')) AS total_colocado
+FROM dim_credit
+WHERE TO_CHAR(origination_date, 'YYYY') = '2024'
+ORDER BY 1
+;
